39cc4de4c4bf381ce4328d59458000002cd358b8
Cuiqing Li <lixx3527@gmail.com>
2017-06-16 17:54:23 -0400

    implemneting prediction function

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,10 +24,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 0.2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -51,8 +51,19 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  //set state dimension
+  n_x = 5;
+  n_aug = 7;
+  //define spreading parameter
+  lambda = 3 - n_aug;
+
 
   is_initialized_=false;
+  p_ << 1,0,0,0,0,
+        0,1,0,0,0,
+        0,0,1,0,0,
+        0,0,0,1,0,
+        0,0,0,0,1;
 }
 
 UKF::~UKF() {}
@@ -76,22 +87,30 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
             */
             double cosval=cos(measurement_pack.raw_measurements_[1]);
             double sinval=sin(measurement_pack.raw_measurements_[1]);
-            ekf_.x_ << measurement_pack.raw_measurements_[0]*cosval, measurement_pack.raw_measurements_[0]*sinval, 0, 0, 0;
-            Hj_=tools.CalculateJacobian(ekf_.x_);
-            ekf_.H_=Hj_;
-            ekf_.R_=R_radar_;
-
+            double ro=meas_package.raw_measurements_[1];
+            x_ << measurement_pack.raw_measurements_[0]*cosval, measurement_pack.raw_measurements_[0]*sinval, 0, ro, 0;
             previous_timestamp_ = measurement_pack.timestamp_;
           }else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-            ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
-            ekf_.H_=H_laser_;
-            ekf_.R_=R_laser_;
+            x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0, 0;
             previous_timestamp_ = measurement_pack.timestamp_;
           }
           is_initialized_ = true;
           return;
         }
 
+        /*****************************************************************************
+        *  Prediction
+        ****************************************************************************/
+        float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+        previous_timestamp_ = measurement_pack.timestamp_;
+
+        Prediction(dt);
+
+        if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+            // Radar updates
+        } else {
+            // Laser updates
+        }
 
 }
 
@@ -102,11 +121,41 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  */
 void UKF::Prediction(double delta_t) {
   /**
-  TODO:
-
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+        //create augmented mean vector
+        VectorXd x_aug = VectorXd(7);
+        //create augmented state covariance
+        MatrixXd P_aug = MatrixXd(7, 7);
+        //create sigma point matrix
+        MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+
+        //create augmented mean state
+        x_aug.head(5) = x;
+        x_aug(5) = 0;
+        x_aug(6) = 0;
+        //create augmented covariance matrix
+       P_aug.fill(0.0);
+       P_aug.topLeftCorner(5,5) = P;
+       P_aug(5,5) = std_a*std_a;
+       P_aug(6,6) = std_yawdd*std_yawdd;
+
+       //create square root matrix
+      MatrixXd L = P_aug.llt().matrixL();
+
+      //create augmented sigma points
+      Xsig_aug.col(0)  = x_aug;
+      for (int i = 0; i< n_aug; i++)
+      {
+        Xsig_aug.col(i+1)       = x_aug + sqrt(lambda+n_aug) * L.col(i);
+        Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda+n_aug) * L.col(i);
+      }
+
+
+
+
+
 }
 
 /**
